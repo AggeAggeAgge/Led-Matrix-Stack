a6fbb42a01847c3e68c3c1a8487e37f2a765fefc
Agge <agaton08olsson@gmail.com>
2025-12-12 15:52:17 +0100

    2025-12-12

--- StackCode/StackCode.ino
+++ StackCode/StackCode.ino
@@ -3,6 +3,8 @@
 
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);  // Display which does not send AC
 
+
+// Declare all variables
 int screenWidth = 64;
 int platformWidth = 20;
 int positionX = 0;
@@ -11,16 +13,20 @@ int speed = 4;
 int buttonPin = 2;
 int platformHeight = 4;
 int level = 0;
+int oldPlatformWidth = 20;
 
 int blockIndex = 0;
 int blockPos[32] = { 0 };
 int blockWidth[32] = { 10 };
 
+
+// function to draw stuff to the oled screen, parameters(a: blockPosition) returns: void 
 void draw(int a) {
 
-  // graphic commands to redraw the complete screen should be placed here
-
+  // Set the font text on the screen for later
   u8g.setFont(u8g_font_unifont);
+
+
   u8g.firstPage();
   do {
     for (int i = 0; i < 32; i++) {
@@ -31,9 +37,10 @@ void draw(int a) {
 }
 
 void setup(void) {
-
+  // Start serial Monitor
   Serial.begin(9600);
 
+  // Assign button Input pin
   pinMode(buttonPin, INPUT);
 
   // assign default color value
@@ -46,27 +53,27 @@ void setup(void) {
   } else if (u8g.getMode() == U8G_MODE_HICOLOR) {
     u8g.setHiColorByRGB(255, 255, 255);
   }
-
-
-  pinMode(8, OUTPUT);
 }
 
 void loop(void) {
   // picture loop
   Serial.println(digitalRead(buttonPin));
 
+  // Draw block att block position
 
-  draw(blockPos);
 
+
+  // if the blocks hits the right wall, flip the direction
   if (blockPos[blockIndex] >= screenWidth - platformWidth) {
     direction = false;
   }
 
+  // if the blocks hits the left wall, flip the direction
   if (blockPos[blockIndex]  <= 0) {
     direction = true;
   }
 
-
+  // Move the block in its designated direction
   if (direction == true) {
     blockPos[blockIndex]  = blockPos[blockIndex]  + speed;
   }
@@ -75,13 +82,17 @@ void loop(void) {
     blockPos[blockIndex]  = blockPos[blockIndex]  - speed;
   }
 
+  draw(blockPos);
+
+
   if (digitalRead(buttonPin) == HIGH) {
     blockIndex = blockIndex + 1;
-    blockPos[blockIndex]  = 0;
+    
+    delay(300);
+    platformWidth -= 4;
   }
-
+  //------------------------
 
 
   // rebuild the picture after some delay
-  delay(50);
 }
